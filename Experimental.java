68ff8258f453865cb9cb11f220ba9d99952827e3
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-11-08 00:32:09 +0300

    Update Experimental.java

--- Experimental.java
+++ Experimental.java
@@ -8,16 +8,16 @@ public class Experimental {
         String input = "\0";
         while (input != "q") {
         input = sc.nextLine();
-            if (input == "w") {
+            if (input.equals("w")) {
             posY++;
             }
-            else if (input == "a") {
+            else if (input.equals("a")) {
             posX--;
             }
-            else if (input == "s") {
+            else if (input.equals("s")) {
                 posY--;
             }
-            else if (input == "d") {
+            else if (input.equals("d")) {
                 posX++;
             }
         else {
@@ -29,26 +29,25 @@ public class Experimental {
                     persiki++;
                     System.out.println("Вы нашли персик! Ваше количество персиков: " + persiki);
                 }
-                if (posY == 43 && posX == 50 || posY ==  79 && posX == 11 && input != "I") {
+                if (posY == 43 && posX == 50 || posY ==  79 && posX == 11 && !input.equals("i")) {
                     input = "R";
-                    System.out.println("Добро пожаловать на рынок! Что вы хотите купить/продать? \n Чтобы что-то купить нажмите B, а чтобы продать нажмите S\n Чтобы выйти, нажмите F");
-                    if (input == "B") {
+                    System.out.println("Добро пожаловать на рынок! Что вы хотите купить/продать? \n Чтобы что-то купить нажмите B, а чтобы продать нажмите S\n Чтобы выйти, нажмите i");
+                    if (input.equals("b") && !input.equals("f") {
                         System.out.println("Машина = 400 монет, персик: 10 монет, телефон: 200 монет\n чтобы купить машину, нажмите M, чтобы купить персик, нажмите P, а чтобы купить телефон нажмите S\n нажмите F для выхода");
                         input = sc.nextLine();
-                        if (input == "m") {
+                        if (input.equals("m")) {
                             if (money >= 400) money -= 400;
                             if (money < 400) System.out.println("Попробуйте продать что-нибудь");
-                            input = "p";
+                            input = "f";
                         }
                         if (input == "p") {
                             if (money >= 10) money -= 10;
-                            if (money < 10) System.out.println("Попробуйте продать что-нибудь");
-                            input = "p";
+                            if (money < 10) System.out.println("Попробуйте продать 
                         }
-                        if (input == "p") {
+                        if (input.equals("p")) {
                             if (money >= 200) money -= 200;
                             if (money < 200) System.out.println("Попробуйте продать что-нибудь");
-                            input = "p";
+                            input = "f";
                         }
                     }
                     if (input == "s") {
