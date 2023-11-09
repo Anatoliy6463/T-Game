b376a00b75e5a0eebd74170c78e9ba1ac3b50146
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-11-09 22:50:50 +0300

    Add files via upload

--- Debug.java
+++ Debug.java
@@ -1,16 +1,10 @@
 import java.util.Scanner;
 public class Debug {
-    public static void main(String[] args) {
-        System.out.println("T-Game 0.0.1");
-        System.out.println("Загрузка...");
-        engine();
-    }
-    public static void engine(){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         double posX = 1;
         double posY = 1;
         int persiki = 100000;
-        char magazin = 'r';
         double hunger = 100;
         String input = "\0";
         while (!input.equals("q")) {
@@ -18,7 +12,7 @@ public class Debug {
         System.out.println("Введите координаты для телепортации");
         posX = sc.nextInt();
         posY = sc.nextInt();
-            System.out.println("X =" + X + "Y =" + Y + "У вас " + persiki + "персиков");
+            System.out.println("X =" + posX + "Y =" + posY + "У вас " + persiki + "персиков");
             input = sc.nextLine();
         if (input.equalsIgnoreCase("e")) {
             persiki--;
