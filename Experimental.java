2845257968a40b9e75226d66bbe56c3c3ed91aeb
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-11-09 22:10:58 +0300

    v0.0.1
    It's a very big update, that returned shop, added hp, sword(useless), armor(useless) and hunger system. Also, peaches are now useful, but, if you go to shop, it's so buggy.

--- Experimental.java
+++ Experimental.java
@@ -1,34 +1,63 @@
 import java.util.Scanner;
+import java.lang.Thread;
 public class Experimental {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("T-Game 0.0.1");
+        System.out.println("Загрузка...");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + '%');
+        }
+        engine();
+    }
+    public static void engine(){
         Scanner sc = new Scanner(System.in);
-        int posX = 0;
-        int posY = 0;
+        double posX = 0;
+        double posY = 0;
         int money = 50;
+        int shopmoney = 0;
         int flag = 0;
+        int persiki = 0;
+        int mech = 0;
+        int armor = 0;
+        int hp = 100;
+        int zapasPersikov = 10000;
+        int counter = 0;
+        char magazin = 'r';
+        double hunger = 0;
         System.out.println("Добро пожаловать в T-Game!");
         String input = sc.nextLine();
         while (input != "q") {
-        if (input.equals("w")) {
+        if (input.equalsIgnoreCase("w")) {
             posY++;
         }
-        else if (input.equals("a")) {
+        else if (input.equalsIgnoreCase("a")) {
             posX--;
         }
-        else if (input.equals("s")) {
+        else if (input.equalsIgnoreCase("s")) {
             posY--;
         }
-        else if (input.equals("d")) {
+        else if (input.equalsIgnoreCase("d")) {
             posX++;
+        }
+        else if (input.equalsIgnoreCase("e") && persiki >= 1) {
+            if (hunger <= 20) hunger = 0;
+        }
+        else if (input.equalsIgnoreCase("e") && persiki < 1) {
+            hunger+=0.25;
+        }
+        else if (input.equalsIgnoreCase("p") && persiki >= 1) {
+            System.out.println("У вас " + persiki + "персиков");
+        }
+        else if (input.equalsIgnoreCase("p") && persiki < 1) {
+            System.out.println("У вас нет персиков, попробуйте найти их или купить в магазине");
         }
         else {
             System.out.println("Нажмите одну из следующих клавиш: W, A, S, D, Q");
         }
         input = sc.nextLine();
-                System.out.println("Ваши координаты: x=" + posX + " y=" + posY + "Количество монет=" + money);
-                int persiki = 0;
+                System.out.println("Ваши координаты: x=" + posX + " y=" + posY + "\nКоличество монет=" + money + " Уровень голода=" + hunger);
                 if (posY % 7 == 0 || posX % 7 == 0 || posY % 7 == 0 && posX % 7 == 0) {
-                    persiki++;
+                    persiki = persiki + 1;
                     System.out.println("Вы нашли персик! Ваше количество персиков: " + persiki);
                 }
                 if (posX % 100 == 0 && posY % 100 == 0 && posX != 0 && posY != 0 && flag == 0) {
@@ -38,10 +67,83 @@ public class Experimental {
                     if (posX % 100 == 0 && posY % 100 == 0 && posX != 0 && posY != 0 && flag == 1) {
                     System.out.println("Здесь уже нет сокровищ");
                 }
-                if (input.equals("q")) {
-                    System.out.println("Спасибо за игру!\n Автор: Анатолий6463");
+                
+                if (posX % 19 == 0 && posY % 19 == 0 && posX != 0 && posY != 0 && magazin == 'r') {
+                    System.out.println("Что вы хотите купить? Меч = S, броня = A, персик = P, чтобы выйти, нажмите Q");
+                    if (input.equalsIgnoreCase("S")) {
+                        mech = 1;
+                        money-=10;
+                        System.out.println("С вас 10 монет");
+                        shopmoney += 10;
+                    }
+                    else if (input.equalsIgnoreCase("A")) {
+                        armor = 1;
+                        money-=20;
+                        System.out.println("С вас 20 монет");
+                        shopmoney += 20;
+                    }
+                    else if (input.equalsIgnoreCase("P") && zapasPersikov > 0) {
+                        System.out.println("Сколько вам персиков?");
+                        int skolko = sc.nextInt();
+                        if (skolko <= zapasPersikov) {
+                            persiki += skolko;
+                            zapasPersikov -= skolko;
+                            money -= 2 * skolko;
+                            shopmoney += 2 * skolko;
+                        }
+                        else if (skolko > zapasPersikov) {
+                            skolko = zapasPersikov - 5;
+                            persiki += skolko;
+                            money -= 2 * persiki;
+                            System.out.println("Извините, у нас было всего лишь " + zapasPersikov + " персиков");
+                            zapasPersikov -= skolko;
+                        }
+                        else if (input.equalsIgnoreCase("Q")) {
+                            magazin = 'v';
+                            posX++;
+                        }
+                    }
+                }
+                if (input.equalsIgnoreCase("q")) {
+                    System.out.println("Закрытие игры");
+                    System.out.println("Готово");
                     sc.close(); 
                     break;
+                }
+                if (posX >= 400 || posY >= 350 || posX <= -400 || posY <= -350 || posX >= 400 && posY >= 350 || posX <= -400 && posY <= -350) {
+                    hp--;
+                }
+                counter++;
+                hunger+=0.15;
+                zapasPersikov++;
+                magazin = 'r';
+                if (hunger >= 100) {
+                    hp--;
+                }
+                if (hunger <= 10) {
+                    hp++;
+                }
+                if (hp >= 100) {
+                    hp = 100;
+                }
+                if (hp <= 0) {
+                    System.out.println("Game over");
+                    money = 0;
+                    mech = 0;
+                    persiki = 0;
+                    System.out.println("Возродиться? Y/n");
+                    if (input.equalsIgnoreCase("Y")) {
+                        posX = 0;
+                        posY = 0;
+                        hp = 100;
+                    }
+                    
+                    else if (input.equalsIgnoreCase("N")) {
+                        System.out.println("Закрытие игры");
+                        System.out.println("Готово");
+                        sc.close();
+                        break;
+                    }
                 }
         }
     }
